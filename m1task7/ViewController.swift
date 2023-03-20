7094e8834a2c06f7205369a43842526edf74e888
Alex Antropoff <alant@idc.md>
2023-03-20 16:32:55 +0200

    Initial Commit

---
+++ m1task7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m1task7
+//
+//  Created by Alex Antropoff on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
